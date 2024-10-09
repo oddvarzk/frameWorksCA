0b5d209ec46f3cb9f8494ff10a122a307bafe134
Oddvar Z Kristiansen <122876488+oddvarzk@users.noreply.github.com>
2024-10-10 00:20:53 +0200

    tailwind styling added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        paleSand: '#F4EFEA',
+        charcoal: '#333333',
+        mutedTerracotta: '#CC7A57',
+        deepBlue: '#4A6FA5',
+        oliveGreen: '#7A8450',
+      },
+    },
   },
   plugins: [],
-};
+}
