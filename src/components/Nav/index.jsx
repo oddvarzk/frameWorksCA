a53ed7458c1ce36526ac1e428f0cfad62cd3a587
Oddvar Z Kristiansen <122876488+oddvarzk@users.noreply.github.com>
2024-10-13 21:37:41 +0200

    minor changes to nav added  (styling)

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+
 
 export function Nav() {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         <nav>
-            <button className="md:hidden p-2 py-5" onClick={() => setIsOpen(!isOpen)}>
+            <button className="md:hidden py-8" onClick={() => setIsOpen(!isOpen)}>
                 <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
                 </svg>
@@ -20,9 +19,6 @@ export function Nav() {
                 </li>
                 <li className="hover:underline">
                     <Link to="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
-                </li>
-                <li className="hover:underline">
-                    <Link to="/cart" onClick={() => setIsOpen(false)}> <FontAwesomeIcon icon={faCartShopping} /> </Link>
                 </li>
             </ul>
         </nav>
