d2a524fdf79d2a253e4537880148e7828513bc2b
Oddvar Z Kristiansen <122876488+oddvarzk@users.noreply.github.com>
2024-10-13 21:37:14 +0200

    rating system added

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import StarRating from '../Rating';
 
 const ProductCard = ({ post }) => {
   const navigate = useNavigate();
@@ -15,8 +16,8 @@ const ProductCard = ({ post }) => {
     >
       <img className='w-52 h-64 shadow-lg' src={post.image.url} alt={post.title} />
       <div>
-        <h3>Rated {post.rating}/5</h3>
-        <h2 className='text-lg py-1'>{post.title}</h2>
+      <StarRating rating={post.rating} />
+        <h2 className='text-lg font-semibold py-1'>{post.title}</h2>
           <div className='flex text-sm justify-between'>
             <h3 className='text-md'>{post.discountedPrice} NOK</h3>
             <h3 className='text-md'>{post.price} NOK</h3>
